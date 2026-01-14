e4b991e1ae4fd6f5540095cb26171a39a0fbce78
Himanshu Yadav ☕️ <himanshuaman92@gmail.com>
2026-01-14 08:14:53 +0000

    multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,16 @@
+# MULTIPLICATTION OF ANY NUMBER 
+
+# first we take one input from the user
+
+# using while loop we print the multiplication
+
+
+num = int(input("CHOOSE ANY NUMBER: "))
+
+i = 1
+
+while (i <= 10 ):
+    print(num,"*", i, "=" ,num*i )
+   
+    i += 1
+
